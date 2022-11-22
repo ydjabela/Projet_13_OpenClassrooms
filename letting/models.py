56e73fb8522eb0bd747d80ba0863e7046719d99e
ydjabela <ydjabela@blendbow.com>
2022-11-22 16:23:39 +0100

    verbose_name_plural

--- letting/models.py
+++ letting/models.py
@@ -10,6 +10,9 @@ class Address(models.Model):
     zip_code = models.PositiveIntegerField(validators=[MaxValueValidator(99999)])
     country_iso_code = models.CharField(max_length=3, validators=[MinLengthValidator(3)])
 
+    class Meta:
+        verbose_name_plural = "Addresses"
+
     def __str__(self):
         return f'{self.number} {self.street}'
 
@@ -17,6 +20,9 @@ class Address(models.Model):
 class Letting(models.Model):
     title = models.CharField(max_length=256)
     address = models.OneToOneField(Address, on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name_plural = "Lettings"
 
     def __str__(self):
         return self.title
