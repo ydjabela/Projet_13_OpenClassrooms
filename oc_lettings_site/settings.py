b5cf5ee6b5ce26768a76e22af209883af0d18b33
ydjabela <ydjabela@blendbow.com>
2022-11-23 14:49:42 +0100

    delete apps conf

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -19,7 +19,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'oc_lettings_site.apps.OCLettingsSiteConfig',
+    #'oc_lettings_site.apps.OCLettingsSiteConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
